5126520a4d7f67a2209a82318507273397e960b1
aeonfluffy <shadow_fox9503@yahoo.com>
2016-06-08 23:42:50 -0500

    Working Map
    Just the boarders and stuff, no actually walls/start/stop implemented

--- board.lua
+++ board.lua
@@ -42,9 +42,11 @@ local y = 0
                     love.graphics.setColor(0, 255, 0)
                 elseif v == 3 then
                     love.graphics.setColor(255, 0, 0)
+                else
+                    love.graphics.setColor(255, 255, 255)
                 end
                 love.graphics.rectangle("fill", x, y, height, width)
-                x = i * 32
+                x = j * 32
         end
         y = i * 32
     end
