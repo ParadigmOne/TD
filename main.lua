33f3799d407d003b6f78a48f2abd6596b6732f20
ParadigmOne <andrew.c.cooper@hotmail.co.uk>
2016-06-10 16:14:09 +0100

    pseudo pathfinding
    runs along a pre-determined path

--- main.lua
+++ main.lua
@@ -3,7 +3,6 @@ require "board"
 require "player"
 
 function love.load()
-	Enemy.findPath()
 	Enemy.spawnNew()
 end
 
@@ -14,5 +13,20 @@ end
 
 function love.draw()
     map.draw()
+	mapdebug()
 	Enemy.draw()
+end
+
+function mapdebug()
+	love.graphics.setColor(255,100,0)
+	for i, v in ipairs(map) do
+		for j, v in ipairs(map[i]) do
+			love.graphics.print(j, (j-1)*32, (i-1)*32)
+			love.graphics.print(i, (j-1)*32, ((i-1)*32)+12)
+		end
+	end
+end
+
+function round(number)
+	return number - (number % 1)
 end
